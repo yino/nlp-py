b587b3c5ca440ca6c60936dddc53f72d31a3e73d
yoloii <bing15829002364@163.com>
2021-07-14 13:53:45 +0800

    submit

--- pro-test.py
+++ pro-test.py
@@ -1,11 +1,16 @@
+from datetime import datetime
 from enum import unique
 from os import name
 from flask import Flask
+from flask.sessions import NullSession
 #from gevent import pywsgi
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
+from sqlalchemy.sql.elements import Null
+from werkzeug.datastructures import UpdateDictMixin
 import config
-from sqlalchemy.orm import backref
+from sqlalchemy.orm import backref, defaultload
+from datetime import datetime
 
 
 pymysql.install_as_MySQLdb()
@@ -25,8 +30,6 @@ class User_appkey(db.Model):
     updated_at = db.Column(db.DateTime(3))
     deleted_at = db.Column(db.DateTime(3))
 
-    users = db.relationship("User", backref = "associated")
-
     def __repr__(self):
         return "User_appkey: %s %s %s %s %s %s %s" % (self.id, self.app_key, self.app_secret, 
                                                         self.user_id, self.created_at, self.updated_at,self.deleted_at)
@@ -34,18 +37,16 @@ class User_appkey(db.Model):
 
 class User(db.Model):
     __tablename__ = "users"
-    id = db.Column(db.BigInteger, primary_key = True)
+    id = db.Column(db.BigInteger, primary_key = True, nullable=False)
     name = db.Column(db.String(255), unique = True)
-    mobile = db.Column(db.BigInteger, unique = True)
-    email = db.Column(db.String(100), unique = True)
-    password = db.Column(db.String(255), unique = True)
-    token = db.Column(db.String(255))
-    token_expire = db.Column(db.DateTime(3))
-    created_at = db.Column(db.DateTime(3))
-    updated_at = db.Column(db.DateTime(3))
-    deleted_at = db.Column(db.DateTime(3))
-
-    associated_id = db.Column(db.String,db.String, db.ForeignKey("user_appkey.app_key", "user_appkey.app_secret"),  nullable=False)
+    mobile = db.Column(db.BigInteger, unique = True, nullable=False)
+    email = db.Column(db.String(100), nullable = True)
+    password = db.Column(db.String(255), unique = True, nullable=False)
+    token = db.Column(db.String(255), nullable = True)
+    token_expire = db.Column(db.DateTime(3), default = datetime.now, nullable=False)
+    created_at = db.Column(db.DateTime(3), default = datetime.now, nullable=False)
+    updated_at = db.Column(db.DateTime(3), default = datetime.now, nullable=False)
+    deleted_at = db.Column(db.DateTime(3), default = datetime.now, nullable = True)
 
     def __repr__(self):
         return "User: %s %s %s %s %s %s %s %s %s %s" % (self.id, self.name, self.mobile, self.email, self.password,
@@ -58,19 +59,17 @@ class Qa_question(db.Model):
     id = db.Column(db.BigInteger)
     question = db.Column(db.String(255), unique = True)
     answer = db.Column(db.String(255), unique = True)
-    pid = db.Column(db.Intege, unique = True)
+    pid = db.Column(db.Integer, unique = True)
     user_id = db.Column(db.BigInteger, primary_key = True)
     type = db.Column(db.BigInteger)
     created_at = db.Column(db.DateTime(3))
     updated_at = db.Column(db.DateTime(3))
     deleted_at = db.Column(db.DateTime(3))
 
-
     def __repr__(self):
         return "Qa_question: %s %s %s %s %s %s %s %s %s" % (self.id, self.question, self.answer, self.pid,
                                                                 self.user_id, self.type, self.created_at,
                                                                 self.updated_at, self.deleted_at)
-
 
 
 @app.route("/")
@@ -82,15 +81,25 @@ if __name__ == '__main__':
 
     #db.drop_all()
     #db.create_all()
-
-    role = Role(name = "yin")
-    db.session.add(role)
-    db.session.commit()
-
-    user = User(name = "ylolii", role_id = role.id)
+ 
+    nowTime = datetime.now()
+    print (nowTime)
+    #print (datetime.strftime(nowTime, '%Y-%m-%d %H:%M:%S.%f'))
+    user = User(name = "yoloii", mobile = "15829002364", email = "123456@163.com", password = "qwertyuiop", token = "whatmean", token_expire = nowTime, 
+                     created_at = nowTime, updated_at = nowTime, deleted_at = nowTime)
     db.session.add(user)
     db.session.commit()
-  
+
+    #删除：查出要删除的数据，拿到Article
+    #result = User.query.filter(User.id==8).first()
+    #db.session.delete(result)
+    #db.session.commit()
+
+    all = User.query.all()
+    print(all)
+    cou = User.query.count()
+    print(cou)
+
     app.run(debug=True)
     #server = pywsgi.WSGIServer(('0.0.0.0',(config.PORT)),app)
     #erver.serve_forever()
