f6d8dc9cd4e993f5a370674eb93c531bb576a204
sunxiaohu <sxh01077484@alibaba-inc.com>
2021-07-30 16:54:51 +0800

    docker

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,7 @@
 workers = 5
 worker_class = "gevent"
 bind = "0.0.0.0:8000"
+errorlog = 'log/gunicorn.error.log'
+accesslog = 'log/gunicorn.access.log'
+access_log_format = '%(h) -  %(t)s - %(u)s - %(s)s %(H)s'
+error_log_format = '%(h) -  %(t)s - %(u)s - %(s)s %(H)s'
