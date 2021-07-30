be546514919606650ee2691374cdfa1619ec9c2c
sunxiaohu <sxh01077484@alibaba-inc.com>
2021-07-30 15:16:30 +0800

    config

--- config/__init__.py
+++ config/__init__.py
@@ -5,7 +5,7 @@ config = configparser.ConfigParser()  # 类实例化
 
 env = os.getenv("ENV")
 env_path = os.getenv("CONFIG_PATH")
-if  env is None or len(env) == 0 or env == '':
+if env is None or len(env) == 0 or env == '':
     filename = "dev.ini"
 else:
     filename = "%s.ini" % (env,)
