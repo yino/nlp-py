1f8b0edcd03acc7967a72c4632c017023be21dc6
sunxiaohu <sxh01077484@alibaba-inc.com>
2021-07-30 15:07:28 +0800

    env update

--- config/__init__.py
+++ config/__init__.py
@@ -3,15 +3,14 @@ import os
 
 config = configparser.ConfigParser()  # 类实例化
 
-def init_config():
-    env = os.getenv("ENV")
-    env_path = os.getenv("CONFIG_PATH")
-    if  env is None or len(env) == 0 or env == '':
-        filename = "dev.ini"
-    else:
-        filename = "%s.ini" % (env,)
-    
-    if  env_path is None or len(env_path) == 0 or env_path == '':
-        env_path = " D:\\project\\nlp-model"
-    config.read(env_path+filename,"utf8")
+env = os.getenv("ENV")
+print(env)
+env_path = os.getenv("CONFIG_PATH")
+if  env is None or len(env) == 0 or env == '':
+    filename = "dev.ini"
+else:
+    filename = "%s.ini" % (env,)
 
+if  env_path is None or len(env_path) == 0 or env_path == '':
+    env_path = " D:\\project\\nlp-model"
+config.read(env_path+filename,"utf8")
