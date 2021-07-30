d7c60d414e1c0f671bea7d68d9629b344d5d9e5a
sunxiaohu <sxh01077484@alibaba-inc.com>
2021-07-30 15:04:17 +0800

    env update

--- applications/__init__.py
+++ applications/__init__.py
@@ -1,3 +1,4 @@
+from config import init_config
 from flask import Flask
 
 from .controller import init_router
@@ -7,6 +8,7 @@ from .model import init_db
 
 def create_app():
     app = Flask(__name__)
+    init_config()
     init_db(app)
     init_router(app)
     init_middleware(app)
--- config/__init__.py
+++ config/__init__.py
@@ -2,9 +2,16 @@ import configparser
 import os
 
 config = configparser.ConfigParser()  # 类实例化
-env = os.getenv("env")
-if  env is None or len(env) == 0 or env == '':
-    filename = "dev.ini"
-else:
-    filename = "%s.ini" % (env,)
-config.read('D:\project\\nlp-model\\'+filename,"utf8")
+
+def init_config():
+    env = os.getenv("ENV")
+    env_path = os.getenv("CONFIG_PATH")
+    if  env is None or len(env) == 0 or env == '':
+        filename = "dev.ini"
+    else:
+        filename = "%s.ini" % (env,)
+    
+    if  env_path is None or len(env_path) == 0 or env_path == '':
+        env_path = " D:\\project\\nlp-model"
+    config.read(env_path+filename,"utf8")
+
