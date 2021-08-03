552ea63575f1f6ff4774b3d325e450c6cb3f5e92
sunxiaohu <sxh01077484@alibaba-inc.com>
2021-08-03 18:11:15 +0800

    Dockerfile

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,4 +5,9 @@ errorlog = 'log/gunicorn.error.log'
 accesslog = 'log/gunicorn.access.log'
 access_log_format = '%(h) -  %(t)s - %(u)s - %(s)s %(H)s'
 error_log_format = '%(h) -  %(t)s - %(u)s - %(s)s %(H)s'
-daemon = settings.DEBUG
+daemon = True
+workers = 4
+# 指定每个工作者的线程数
+threads = 2
+# 设置最大并发量
+worker_connections = 2000
