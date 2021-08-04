92d314731631791e2e325d296a6728eef081b041
sunxiaohu <sxh01077484@alibaba-inc.com>
2021-08-04 14:01:29 +0800

    Dockerfile

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def status():
     return "ok"
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('', config["app"]["PORT"]), app)
+    http_server = WSGIServer(('', int(config["app"]["PORT"])), app)
     http_server.serve_forever()
     # app.run(debug=config['app']['DEBUG'],host=config["app"]["HOST"], port=config["app"]["PORT"])
 
